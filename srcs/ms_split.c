ee18c3bc478f447df3795012317a4ba3e69c0455
antonio <anto.pintauro1@gmail.com>
2025-03-31 18:04:32 +0200

    fix: fixed the ms_split to make it handles the quotes correctly with an helper function; fixed some others minimal tiping errors. Now it should work, it neeeds a review.

--- srcs/ms_split.c
+++ srcs/ms_split.c
@@ -3,19 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ms_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahabdelr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: apintaur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 14:47:50 by ahabdelr          #+#    #+#             */
-/*   Updated: 2025/03/19 17:13:50 by ahabdelr         ###   ########.fr       */
+/*   Updated: 2025/03/31 17:58:52 by apintaur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdlib.h>
 
-static size_t	ms_wordcounter(const char *s, const char c);
-static char		**ms_ssalloc(const char *s, const char c);
+char			**ms_misery_init(size_t *i, size_t *k, const char *s, char c);
 static char		**ms_salloc(char **ss, const char *s, size_t *k, size_t cnt);
+void			ms_counter(char s, char *open, size_t *cnt, int *i);
+static size_t	ms_wordcounter(const char *s, const char c);
+int				ms_split_checks(char *s, char *open, int *i);
+char			**ms_ssalloc(const char *s, const char c);
 static void		*ms_ssdestroy(char **ss);
 
 char	**ms_split(const char *s, char c)
@@ -33,20 +36,21 @@ char	**ms_split(const char *s, char c)
 	while (s[i] != '\0')
 	{
 		cnt = 0;
-		if (s[i] == c)
+		if (s[i] == c && open == '\0')
 			i++;
 		else
 		{
-			while ((s[i + cnt] != '\0' && s[i + cnt] != c) || open != '\0')
-				ms_counter(s[i + cnt], &open, &cnt, &i);
-			if (ms_salloc(ss, &(s[i]), &k, cnt) == NULL)
+			while ((s[i + cnt] != '\0' && (s[i + cnt] != c || open != '\0')))
+				ms_counter(s[i + cnt], &open, &cnt, (int *)&i);
+			if (cnt > 0 && ms_salloc(ss, &(s[i]), &k, cnt) == NULL)
 				return (ms_ssdestroy(ss));
+			i += cnt;
 		}
-		i += cnt;
 	}
+	if (open != '\0')
+		ms_wait_for_eof((char *)s, &open, NULL);
 	return (ss);
 }
-
 
 static size_t	ms_wordcounter(const char *s, const char c)
 {
@@ -59,19 +63,19 @@ static size_t	ms_wordcounter(const char *s, const char c)
 	open = '\0';
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
+		if (s[i] == c && open == '\0')
 			i++;
 		else
 		{
 			words++;
-			while ((s[i] != '\0' && s[i] != c) || open != '\0')
-				ms_split_checks(s[i], &open, &i);
+			while (s[i] != '\0' && (s[i] != c || open != '\0'))
+				ms_split_checks((char *)s, &open, (int *)&i);
 		}
 	}
 	return (words);
 }
 
-static char	**ms_ssalloc(const char *s, const char c)
+char	**ms_ssalloc(const char *s, const char c)
 {
 	char	**ss;
 	size_t	n;
@@ -79,7 +83,7 @@ static char	**ms_ssalloc(const char *s, const char c)
 	if (!s)
 		return (NULL);
 	n = ms_wordcounter(s, c);
-	ss = (char **) malloc(sizeof(char *) * (n + 1));
+	ss = (char **)malloc(sizeof(char *) * (n + 1));
 	if (ss == NULL)
 		return (NULL);
 	ss[n] = NULL;
@@ -90,25 +94,21 @@ static char	**ms_salloc(char **ss, const char *s, size_t *k, size_t cnt)
 {
 	size_t	i;
 	char	open;
-	int	j;
 
-	j = 0;
 	open = '\0';
 	i = 0;
 	if (s == NULL || cnt == 0)
 		return (NULL);
-	ss[*k] = (char *) malloc(sizeof(char) * (cnt + 1));
+	ss[*k] = (char *)malloc(sizeof(char) * (cnt + 1));
 	if (ss[*k] == NULL)
 		return (NULL);
 	while (i < cnt)
 	{
-		if ((s[i] == '"' || s == '\'') && open == '\0')
+		if ((s[i] == '"' || s[i] == '\'') && open == '\0')
 			open = s[i];
 		else if (s[i] == open && open != '\0')
 			open = '\0';
-		if (!(s[i] == open && open != '\0'))
-			j++;
-		ss[*k][i] = s[i + j];
+		ss[*k][i] = s[i];
 		i++;
 	}
 	ss[*k][i] = '\0';
--- srcs/ms_split_utils.c
+++ srcs/ms_split_utils.c
@@ -3,52 +3,103 @@
 /*                                                        :::      ::::::::   */
 /*   ms_split_utils.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahabdelr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: apintaur <marvin@42.fr>                    +#+  +:+         +:+     */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 15:40:43 by ahabdelr          #+#    #+#             */
-/*   Updated: 2025/03/19 17:14:17 by ahabdelr         ###   ########.fr       */
+/*   Updated: 2025/03/25 11:50:18 by apintaur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
 #include <stddef.h>
 #include <stdlib.h>
+
+static int	ms_wait_for_eof(char *s, char *open, int *i);
+char		**ms_ssalloc(const char *s, const char c);
 
 void	ms_counter(char s, char *open, size_t *cnt, int *i)
 {
 	if (s == '\0')
-	// funzione per aspettare l'unquote
+		return ;
 	if ((s == '"' || s == '\'') && *open == '\0')
 	{
 		*open = s;
-		(*i)++;
+		(*cnt)++;
 	}
 	else if (s == *open && *open != '\0')
 	{
 		*open = '\0';
-		(*i)++;
+		(*cnt)++;
 	}
 	else
 		(*cnt)++;
 }
 
-char	**ms_misery_init(size_t *i, size_t* k, const char* s, char c)
+char	**ms_misery_init(size_t *i, size_t *k, const char *s, char c)
 {
 	*i = 0;
 	*k = 0;
 	return (ms_ssalloc(s, c));
 }
 
-void	ms_split_checks(char s, char *open, int *i)
+int	ms_split_checks(char *s, char *open, int *i)
 {
-	if (s == '\0')
-		// funzione per aspettare l'unquote
-	if ((s == '"' || s == '\'') && *open == '\0')
-	{
-		*open = s;
-	}
-	else if (s == *open && *open != '\0')
-	{
+	if (s[*i] == '\0')
+		return (ms_wait_for_eof(s, open, i));
+	else if (!ft_strncmp(&(s[*i]), "<<", 2))
+		return (ms_wait_for_eof(s, NULL, i));
+	else if ((s[*i] == '"' || s[*i] == '\'') && *open == '\0')
+		*open = s[*i];
+	else if (s[*i] == *open && *open != '\0')
 		*open = '\0';
-	}
 	(*i)++;
+	return (0);
+}
+
+static char	*ms_set_waitingparams(char *s, int *i, char **kw, char **endl)
+{
+	char	*tmp;
+
+	if (i == NULL)
+	{
+		*kw = "quote> ";
+		*endl = ft_strdup(s);
+	}
+	else
+	{
+		*kw = "heredoc> ";
+		tmp = ft_strchr(&(s[*i]), ' ');
+		if (tmp == NULL)
+			tmp = ft_strchr(&(s[*i]), '\0');
+		*endl = ft_substr(s, *i + 2, tmp - &(s[*i]) - 2);
+	}
+	return (ft_strdup(""));
+}
+
+static int	ms_wait_for_eof(char *s, char *open, int *i)
+{
+	char	*line;
+	char	*joined;
+	char	*keyword;
+	char	*endline;
+
+	if ((!open && !i) || !s)
+		return (0);
+	joined = ms_set_waitingparams(s, i, &keyword, &endline);
+	line = readline(keyword);
+	while (line)
+	{
+		if (ft_strnstr(line, endline, ft_strlen(endline)))
+		{
+			joined = ms_strnjoin(joined, line, ft_strlen(line));
+			break ;
+		}
+		joined = ms_strnjoin(joined, line, ft_strlen(line));
+		free(line);
+		line = readline(keyword);
+	}
+	free(line);
+	free(endline);
+	free(joined);
+	return (1);
 }

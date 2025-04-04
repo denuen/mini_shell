82fb8522a5f5b466e68c036ffd01789a579ac9e9
Ahmed Abdelrahman <ahabdelr@e4r5p1.42roma.it>
2025-04-04 14:52:13 +0200

    fix: visualization now works as it should

--- srcs/built-ins/pwd.c
+++ srcs/built-ins/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahabdelr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: marvin@42.fr <ahabdelr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 19:55:37 by ahabdelr          #+#    #+#             */
-/*   Updated: 2025/03/12 13:26:13 by ahabdelr         ###   ########.fr       */
+/*   Updated: 2025/04/04 14:40:38 by marvin@42.f      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,10 @@ int	ms_pwd(t_node *node)
 	if (path)
 	{
 		ft_output(path);
+		write(1, "\n", 1);
 		return (0);
 	}
+	write(1, "\n", 1);
 	ft_output_error(node->cmd[0], " ");
 	return (-1);
 }

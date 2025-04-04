f3b2ccbb1dd1b07222e8765d422d3e8c51b088d2
Ahmed Abdelrahman <ahabdelr@e4r5p1.42roma.it>
2025-04-04 14:59:22 +0200

    fix: pwd

--- srcs/built-ins/pwd.c
+++ srcs/built-ins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: marvin@42.fr <ahabdelr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 19:55:37 by ahabdelr          #+#    #+#             */
-/*   Updated: 2025/04/04 14:40:38 by marvin@42.f      ###   ########.fr       */
+/*   Updated: 2025/04/04 14:58:00 by marvin@42.f      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ms_pwd(t_node *node)
 	path = getcwd(NULL, 0);
 	if (path)
 	{
-		ft_output(path);
+		write(1, path, ft_strlen(path));
 		write(1, "\n", 1);
 		return (0);
 	}

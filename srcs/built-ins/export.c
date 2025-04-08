f99ef31bb44c255d1daed003c67abdd23a77e44f
denuen <anto.pintauro1@gmail.com>
2025-04-08 14:21:38 +0200

    fix: fixed export.

--- srcs/built-ins/export.c
+++ srcs/built-ins/export.c
@@ -6,36 +6,36 @@
 /*   By: apintaur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 16:14:35 by ahabdelr          #+#    #+#             */
-/*   Updated: 2025/04/03 14:22:12 by apintaur         ###   ########.fr       */
+/*   Updated: 2025/04/08 14:17:01 by apintaur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	ft_env_arg(char *name, t_node *node, t_minishell *ms)
+int	ft_env_arg(char *name, char *value, t_minishell *ms)
 {
-	t_env	*env;
-	t_env	*var;
+	t_env	*tmp;
 
-	env = ft_env_find(ms->envs, name);
-	if (env)
+	tmp = ft_env_find(ms->envs, name);
+	if (tmp && value)
 	{
-		if (node->cmd[2])
-			env->value = node->cmd[2];
+		if (tmp->value)
+			free (tmp->value);
+		tmp->value = value;
 	}
 	else
 	{
-		var = ft_env_find(ms->vars, name);
-		if (var)
+		tmp = ft_env_find(ms->vars, name);
+		if (tmp)
 		{
-			var = ft_new_env(var->name, var->value);
-			ft_env_addordered(&ms->envs, var);
+			tmp = ft_new_env(tmp->name, tmp->value);
+			ft_env_addordered(&ms->envs, tmp);
 			return (0);
 		}
 		else
 		{
-			var = ft_new_env(name, node->cmd[2]);
-			ft_env_addordered(&ms->envs, var);
+			tmp = ft_new_env(name, value);
+			ft_env_addordered(&ms->envs, tmp);
 		}
 	}
 	return (1);
@@ -43,9 +43,18 @@ int	ft_env_arg(char *name, t_node *node, t_minishell *ms)
 
 int	ms_export(t_node *node, t_minishell *ms)
 {
+	char	*name;
+	char	*value;
+
 	if (node->cmd[1] == NULL)
 		ft_env_print(ms->envs);
 	else
-		ft_env_arg(node->cmd[1], node, ms);
+	{
+		name = ft_substr(node->cmd[1], 0, ft_findchr(node->cmd[1], '='));
+		value = ft_substr(node->cmd[1], ft_findchr(node->cmd[1], '=') + 1, ft_strlen(node->cmd[1]));
+		ft_env_arg(name, value, ms);
+		free (name);
+		free (value);
+	}
 	return (0);
 }

53d1e039cfb464bcccc442ad2b1c2caa91b8f881
ahmeee-dev <ahmeabd36@gmail.com>
2025-04-10 16:24:27 +0200

    feat: added safe sgl handle

--- srcs/executor/operators.c
+++ srcs/executor/operators.c
@@ -15,45 +15,38 @@
 
 int	ms_or_exec(t_node *left, t_node *right, t_minishell *ms)
 {
-	int	status;
-
-	sig_check();
-	status = ms_executor(left, ms);
-	sig_check();
-	if (status != 0)
-		status = ms_executor(right, ms);
-	return (status);
+	if (sgl == 0)
+		sgl = ms_executor(left, ms);
+	if (sgl != 0)
+		sgl = ms_executor(right, ms);
+	return (sgl);
 }
 
 int	ms_bg_exec(t_node *left, t_node *right, t_minishell *ms)
 {
 	pid_t	pid;
-	int	status[2];
+	int	status;
 
-	status[1] = 0;
+	status = 0;
 	pid = fork();
 	if (pid == 0)
 	{
-		sig_check();
-		status[0] = ms_executor(left, ms);
-		exit(status[0]);
+		status = ms_executor(left, ms);
+		exit(status);
 	}
-	sig_check();
-	status[1] = ms_executor(right, ms);
-	wait(&status[0]);
-	status[0] = WEXITSTATUS(status[0]);
-	return (status[0] + status[1]);
+	sgl = ms_executor(right, ms);
+	wait(&status);
+	status = WEXITSTATUS(status);
+	return (sgl);
 }
 
 int	ms_pipe_exec(t_node *left, t_node *right, t_minishell *ms)
 {
 	int	pipefd[2];
 	pid_t	pid;
-	int	status[2];
 	int	saved;
 
 	saved = dup(STDIN_FILENO);
-	status[1] = 0;
 	pipe(pipefd);
 	pid = fork();
 	if (pid == 0)
@@ -61,40 +54,35 @@ int	ms_pipe_exec(t_node *left, t_node *right, t_minishell *ms)
 		close(pipefd[0]);
 		dup2(pipefd[1], STDOUT_FILENO);
 		close(pipefd[1]);
-		sig_check();
-		status[0] = ms_executor(left, ms);
-		exit(status[0]);
+		sgl = ms_executor(left, ms);
+		exit(sgl);
 	}
 	dup2(pipefd[0], STDIN_FILENO);
 	close(pipefd[0]);
 	close(pipefd[1]);
-	wait(&status[0]);
-	status[0] = WEXITSTATUS(status[0]);
-	sig_check();
-	status[1] = ms_executor(right, ms);
+	wait(&sgl);
+	sgl = WEXITSTATUS(sgl);
+	if (sgl == 0)
+		sgl = ms_executor(right, ms);
 	dup2(saved, STDIN_FILENO);
 	close(saved);
-	return (status[1]);
+	return (sgl);
 }
 
 int	ms_and_operator(t_node *left, t_node *right, t_minishell *ms)
 {
 	pid_t	pid;
-	int	status[2];
 
-
-	status[1] = 0;
+	sgl = 0;
 	pid = fork();
 	if (pid == 0)
 	{
-		status[0] = ms_executor(left, ms);
-		sig_check();
-		exit(status[0]);
+		sgl = ms_executor(left, ms);
+		exit(sgl);
 	}
-	wait(&status[0]);
-	status[0] = WEXITSTATUS(status[0]);
-	sig_check();
-	if (status[0] == 0)
-		status[1] = ms_executor(right, ms);
-	return (status[0] + status[1]);
+	wait(&sgl);
+	sgl = WEXITSTATUS(sgl);
+	if (sgl == 0)
+		sgl = ms_executor(right, ms);
+	return (sgl);
 }
--- srcs/executor/redirections.c
+++ srcs/executor/redirections.c
@@ -17,7 +17,6 @@ int	ms_tofile_exec(t_node *left, t_minishell *ms, char *file)
 {
 	int	fd;
 	pid_t	pid;
-	int	status;
 
 	pid = fork();
 	if (pid == 0)
@@ -27,21 +26,19 @@ int	ms_tofile_exec(t_node *left, t_minishell *ms, char *file)
 			exit(-1);
 		dup2(fd, STDOUT_FILENO);
 		sig_check();
-		status = ms_executor(left, ms);
+		sgl = ms_executor(left, ms);
 		close(fd);
-		exit(status);
+		exit(sgl);
 	}
-	wait(&status);
-	sig_check();
-	status = WEXITSTATUS(status);
-	return (status);
+	wait(&sgl);
+	sgl = WEXITSTATUS(sgl);
+	return (sgl);
 }
 
 int	ms_fromfile_exec(t_node *left, t_minishell *ms, char *file)
 {
 	int	fd;
 	pid_t	pid;
-	int	status;
 
 	pid = fork();
 	if (pid == 0)
@@ -51,20 +48,18 @@ int	ms_fromfile_exec(t_node *left, t_minishell *ms, char *file)
 			exit(-1);
 		dup2(fd, STDIN_FILENO);
 		sig_check();
-		status = ms_executor(left, ms);
+		sgl = ms_executor(left, ms);
 		close(fd);
-		exit(status);
+		exit(sgl);
 	}
-	wait(&status);
-	sig_check();
-	status = WEXITSTATUS(status);
-	return (status);
+	wait(&sgl);
+	sgl = WEXITSTATUS(sgl);
+	return (sgl);
 }
 
 int	ms_append_exec(t_node *left, t_minishell *ms, char *file)
 {
 	pid_t	pid;
-	int	status;
 	int	fd;
 
 	pid = fork();
@@ -72,22 +67,20 @@ int	ms_append_exec(t_node *left, t_minishell *ms, char *file)
 	{
 		fd = open(file, O_WRONLY | O_APPEND | O_CREAT, 0644);
 		if (fd < 0)
-			status = -1;
+			sgl = -1;
 		dup2(fd, STDOUT_FILENO);
 		close(fd);
 		sig_check();
-		status = ms_executor(left, ms);
-		exit(status);
+		sgl = ms_executor(left, ms);
+		exit(sgl);
 	}
-	wait(&status);
-	status = WEXITSTATUS(status);
-	sig_check();
-	return (status);
+	wait(&sgl);
+	sgl = WEXITSTATUS(sgl);
+	return (sgl);
 }
 
 int	ms_heredoc_exec(t_node *left, t_minishell *ms, char *file)
 {
-	int	status;
 	pid_t	pid;
 	int	fd[2];
 	int	saved;
@@ -111,10 +104,9 @@ int	ms_heredoc_exec(t_node *left, t_minishell *ms, char *file)
 		dup2(fd[0], STDIN_FILENO);
 		close(fd[0]);
 		close(fd[1]);
-		wait(&status);
-		sig_check();
-		status = ms_executor(left, ms);
+		wait(&sgl);
+		sgl = ms_executor(left, ms);
 		dup2(saved, STDIN_FILENO);
 	}
-	return (status);
+	return (sgl);
 }
